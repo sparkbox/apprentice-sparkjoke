77d28ce2c2d9f9d9e375b3c6521c741cd420c507
Osmond Arnesto <53844657+arnest00@users.noreply.github.com>
2022-02-09 12:09:18 -0700

    chore: Remove linter warnings

--- api/routes.js
+++ api/routes.js
@@ -1,36 +1,30 @@
-"use strict";
-
-const Hapi = require("@hapi/hapi");
+const Hapi = require('@hapi/hapi');
 
 const init = async () => {
   const server = Hapi.server({
     port: 8081,
-    host: "localhost",
+    host: 'localhost',
   });
 
   server.route({
-    method: "GET",
-    path: "/",
-    handler: (request, h) => {
-      return "Home";
-    },
+    method: 'GET',
+    path: '/',
+    handler: () => 'Home',
   });
+
   server.route({
-    method: "GET",
-    path: "/welcome",
-    handler: (request, h) => {
-      return "Hello World!";
-    },
+    method: 'GET',
+    path: '/welcome',
+    handler: () => 'Hello World!',
   });
 
   await server.start();
-  console.log("Server running on %s", server.info.uri);
+  console.log('Server running on %s', server.info.uri);
 };
 
-process.on("unhandledRejection", (err) => {
+process.on('unhandledRejection', (err) => {
   console.log(err);
   process.exit(1);
 });
 
 init();
-
