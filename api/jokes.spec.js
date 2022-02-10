858c0e10e6957cfba03410650858878a5df373c6
Osmond <osmond@heysparkbox.com>
2022-02-10 14:39:16 -0700

    test: Add tests for jokes.js
    References FSA22V1-72

---
+++ api/jokes.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import { getJokes } from './jokes';
+
+describe('getJokes', () => {
+  it('returns joke at index 12 when passing in 12 as argument', () => {
+    const joke = getJokes(12);
+    expect(joke.setup).toBe('How many apples grow on a tree?');
+  });
+});
