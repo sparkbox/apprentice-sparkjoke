0e939b5430853687dc4dc71d24a33a86a4f3a040
Theo Gainey <theo@heysparkbox.com>
2022-02-14 10:00:34 -0500

    feat: dynamically render hard coded joke
    remove hard coded joke from index.html
    
    add function to dynamically render hard coded joke to DOM
    
    References #FSA22V1-74
    
    On branch feat--onLoad-renderJoke
     Changes to be committed:
     modified:   src/html/index.html
     modified:   src/js/index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
 import '../css/index.css';
+
+// hard coded joke object
+const hardcodedJoke = {
+  setup: 'Why did the chicken cross the road?',
+  punchline: 'To get to the other side.',
+};
+
+// dynamically renders joke to DOM
+// uses hardcoded joke as default parameter
+export const renderJoke = (jokeObject = hardcodedJoke) => {
+  // get DOM elements
+  const setupDiv = document.getElementById('setup');
+  const punchlineDiv = document.getElementById('punchline');
+
+  // set new text for Divs
+  setupDiv.innerText = jokeObject.setup;
+  punchlineDiv.innerText = jokeObject.punchline;
+};
+
+// testing fails without set timeout
+// set timeout to 0 puts reneder joke on the task quque.
+setTimeout(() => renderJoke(), 0);
