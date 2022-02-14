2f3a225c9f623afb370990972e77c3b4b8fedb1f
Meg Howe <meg@heysparkbox.com>
2022-02-14 13:26:41 -0500

    fix: correct global .gitignore path, do not need ~

--- api/routes.js
+++ api/routes.js
@@ -1,31 +1,31 @@
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
 
 const init = async () => {
   const server = Hapi.server({
     port: 8081,
-    host: 'localhost',
+    host: "localhost",
   });
 
   server.route({
-    method: 'GET',
-    path: '/welcome',
-    handler: () => 'Hello World!!',
+    method: "GET",
+    path: "/welcome",
+    handler: () => "Hello World!!",
   });
 
   server.route({
-    method: 'GET',
-    path: '/jokes',
+    method: "GET",
+    path: "/jokes",
     handler: () => ({
-      setup: 'Joke setup',
-      punchline: 'Joke punchline',
+      setup: "Joke setup",
+      punchline: "Joke punchline",
     }),
   });
 
   await server.start();
-  console.log('Server running on %s', server.info.uri);
+  console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
