cbc2b9a4af891c70253793f7f11a04dab3310985
Meg Howe <meg@heysparkbox.com>
2022-02-17 17:09:28 -0500

    fix: change length of jokes to upperBound of jokes
    info:
    -I changed the function to be jokes.length - 1 to more literally reflect the card
    -I have not changed the routes since my last commit of them, because all that was needed to remove the error was the differing JS synatx
    -(i.e. module.exports = getUpperBound(); )
    -When you run the server on this route, you will get 29, as it is the number of jokes minus 1
    
    FSA22V1-76

--- src/Utilities/upperBound.js
+++ src/Utilities/upperBound.js
@@ -2,7 +2,7 @@ const jokeData = require('../../api/jokes.json');
 
 function getUpperBound() {
   console.log(jokeData.length);
-  return jokeData.length;
+  return jokeData.length - 1;
 }
 
 module.exports = getUpperBound();
