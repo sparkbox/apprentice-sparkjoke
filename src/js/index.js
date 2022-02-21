7683e6b1fe657c2e42f90d6a64550ea10e763193
Osmond <osmond@heysparkbox.com>
2022-02-21 12:58:37 -0700

    feat: Add support for refreshing jokes
    - Change name of `main` function to `showCurrentJoke` for clarity
    - Create static constant as placeholder for upper bound of jokes
    - `bindEventListenerToRefreshButton` function handles state of current
    joke

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,33 @@
 import '../css/index.css';
 import { jokeFetcher } from './jokeFetcher';
 import { renderJoke } from './renderJoke';
+import { getNextJokeIdx } from './getNextJokeIdx';
 
-const main = async () => {
-  const joke = await jokeFetcher();
-  renderJoke(joke);
+const UPPER_BOUND = 4;
+
+const addNextJoke = async (nextJokeIdx) => {
+  const nextJoke = await jokeFetcher(nextJokeIdx);
+
+  renderJoke(nextJoke);
 };
-main();
+
+const bindEventListenerToRefreshButton = (currentJokeIdx) => {
+  const refreshButton = document.getElementById('refresh');
+  let currentJokeState = currentJokeIdx;
+
+  refreshButton.addEventListener('click', async (e) => {
+    e.preventDefault();
+    currentJokeState = getNextJokeIdx(currentJokeState, UPPER_BOUND);
+    addNextJoke(currentJokeState);
+  });
+};
+
+const showCurrentJoke = async () => {
+  const currentJokeIdx = 0;
+  const currentJoke = await jokeFetcher(currentJokeIdx);
+
+  renderJoke(currentJoke);
+  bindEventListenerToRefreshButton(currentJokeIdx);
+};
+
+showCurrentJoke();
