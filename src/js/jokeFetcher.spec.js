93812fcf5ae3780b3484c66ecae09a6c66eb009f
Theo Gainey <theo@heysparkbox.com>
2022-02-23 11:03:56 -0500

    test: test jokeFetcherUpperBounds
    add tests for jokeFetcherUpperBounds
    
    references #FSA22V1-78
    
    On branch feat--fetch-upperbound
     Changes to be committed:
     modified:   src/js/jokeFetcher.spec.js

--- src/js/jokeFetcher.spec.js
+++ src/js/jokeFetcher.spec.js
@@ -1,21 +1,21 @@
-import { jokeFetcher } from './jokeFetcher';
+import { jokeFetcher, jokeFetcherUpperBounds } from './jokeFetcher';
 
 const defaultFetchReference = global.fetch;
 
-beforeEach(() => {
-  global.fetch = jest.fn(() => Promise.resolve({
-    json: () => Promise.resolve({
-      setup: 'Why did the chicken cross the playground?',
-      punchline: 'To get to the other slide.',
-    }),
-  }));
-});
-
-afterEach(() => {
-  global.fetch = defaultFetchReference;
-});
-
 describe('jokeFetcher', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve({
+        setup: 'Why did the chicken cross the playground?',
+        punchline: 'To get to the other slide.',
+      }),
+    }));
+  });
+
+  afterEach(() => {
+    global.fetch = defaultFetchReference;
+  });
+
   it('fetches joke', async () => {
     const joke = await jokeFetcher(0);
     expect(joke).toEqual({
@@ -26,7 +26,6 @@ describe('jokeFetcher', () => {
 
   it('makes a call to the correct API endpoint', async () => {
     await jokeFetcher(3);
-
     expect(fetch).toHaveBeenCalledWith('http://localhost:8081/jokes/3');
   });
 
@@ -37,5 +36,28 @@ describe('jokeFetcher', () => {
       setup: 'Sorry, we were unable to retrieve a joke',
       punchline: 'Sadly this is not a joke',
     });
+  });
+});
+
+describe('jokeFetcherUpperBounds', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(29),
+    }));
+  });
+
+  afterEach(() => {
+    global.fetch = defaultFetchReference;
+  });
+
+  it('returns a number', async () => {
+    const UPPER_BOUND = await jokeFetcherUpperBounds();
+    expect(UPPER_BOUND).toEqual(29);
+  });
+
+  it('returns a fallback number on failure', async () => {
+    fetch.mockImplementationOnce(() => Promise.reject(new Error('Some Failure')));
+    const UPPER_BOUND = await jokeFetcherUpperBounds();
+    expect(UPPER_BOUND).toEqual(0);
   });
 });
